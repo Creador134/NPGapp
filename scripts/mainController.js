72a08e4f247dd1185f81e2dba082a04fa7f61a6c
Jonas Windmolders <jonas.windmolders@saga.be>
2015-01-23 16:48:55 +0100

    0.1.2

--- scripts/mainController.js
+++ scripts/mainController.js
@@ -5,8 +5,18 @@
 NPG.controller('mainController',['$rootScope','$scope','$interval',
     function($rootScope,$scope,$interval) {
 
-       var MC = $scope;
-       var RC = $rootScope;
+        var MC = $scope;
+        var RC = $rootScope;
+
+        MC.servers = [
+            {game: "Arma 3" , mod: "Epoch", name : "NPG Overpoch Chernarus 1", ip: "37.187.135.190" , port : "5102" , namesmall:"A3 Epoch"} ,
+            {game: "Arma 2" ,mod: "DayZ", name : "NPG DayZ PVP 1", ip: "37.187.77.74" , port : "2342", namesmall:"DayZ PVP1"} ,
+            {game: "Arma 2" ,mod: "DayZ", name : "NPG DayZ PVP 2", ip: "37.187.77.74" , port : "2452", namesmall:"DayZ PVP2"} ,
+            {game: "Arma 2" ,mod: "DayZ", name : "NPG DayZ PVE 1", ip: "37.187.77.74" , port : "2302", namesmall:"DayZ PVE1"} ,
+            {game: "Arma 2" ,mod: "DayZ Overwatch", name : "NPG Overwatch 1", ip: "37.187.135.190" , port : "2312" , namesmall:"Overwatch" }
+        ];
+
+        MC.ssi = 0;
 
         MC.openLink = openExternalURL;
 
